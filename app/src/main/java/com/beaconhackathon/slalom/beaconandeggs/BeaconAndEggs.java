1d908b7777ead78303584f5af79145f21b7638f2
icburns <ebeptonian@gmail.com>
2015-10-21 16:37:18 -0700

    cleaning up BeaconAndEggs.java from merge

--- app/src/main/java/com/beaconhackathon/slalom/beaconandeggs/BeaconAndEggs.java
+++ app/src/main/java/com/beaconhackathon/slalom/beaconandeggs/BeaconAndEggs.java
@@ -11,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -21,11 +20,6 @@ import android.widget.TextView;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
-=======
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ListView;
->>>>>>> 7df7e18e2eae0f1dd47824f83289f30af1f97e47
 import android.widget.Toast;
 import org.json.*;
 
@@ -55,16 +49,10 @@ public class BeaconAndEggs extends Activity {
 
     private Notifications notifications;
 
-<<<<<<< HEAD
     private ListViewAdapter mListViewAdapter;
 
     private Context mContext = this;
-=======
->>>>>>> 7df7e18e2eae0f1dd47824f83289f30af1f97e47
 
-    private ListViewAdapter mListViewAdapter;
-
-    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +69,6 @@ public class BeaconAndEggs extends Activity {
         mListViewAdapter = new ListViewAdapter(this, groceryCart);
 
         groceryListView.setAdapter(mListViewAdapter);
-<<<<<<< HEAD
 
         mListViewAdapter.setMode(Attributes.Mode.Single);
 
@@ -118,44 +105,6 @@ public class BeaconAndEggs extends Activity {
             }
         });
 
-=======
-
-        mListViewAdapter.setMode(Attributes.Mode.Single);
-
-        groceryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ((SwipeLayout) (groceryListView.getChildAt(
-                        position - groceryListView.getFirstVisiblePosition()))).open(true);
-            }
-        });
-        groceryListView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Log.e("ListView", "OnTouch");
-                return false;
-            }
-        });
-        groceryListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(mContext, "OnItemLongClickListener", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        groceryListView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-                Log.e("ListView", "onScrollStateChanged");
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-
-            }
-        });
-
->>>>>>> 7df7e18e2eae0f1dd47824f83289f30af1f97e47
         groceryListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
